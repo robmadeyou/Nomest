ffff06808833ca5e96385daac1882667841db1f0
robmadeyou <robmadeyou@gmail.com>
2015-01-30 18:36:47 +0000

    Updated Abereth and starting work on the level editor

---
+++ src/com/gmail/robmadeyou/leveleditor/Editor.java
@@ -0,0 +1,8 @@
+package com.gmail.robmadeyou.leveleditor;
+
+/**
+ * Created by exfos on 29/01/15.
+ */
+public class Editor
+{
+}
---
+++ src/com/gmail/robmadeyou/models/Room.java
@@ -0,0 +1,8 @@
+package com.gmail.robmadeyou.models;
+
+/**
+ * Created by exfos on 29/01/15.
+ */
+public class Room
+{
+}
